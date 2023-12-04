70cc203b6d8c74ef8d947687dc2d415c6631abab
Sandip Sharma <112794092+sandip-0206@users.noreply.github.com>
2023-12-04 16:54:52 +0530

    Complete create listing page UI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -13,6 +13,7 @@ import {
   signOutUserFailure,
   signOutUserSuccess} from "../redux/user/userSlice.js";
 import { useDispatch } from "react-redux";
+import {Link} from 'react-router-dom'
 export default function Profile() {
   const fileRef = useRef(null)
   const {currentUser, loading, error} = useSelector((state) => state.user)
@@ -163,6 +164,11 @@ const handleSignOut = async () => {
           rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80">
             {loading ? 'Loading' : 'Update'}
         </button>
+        <Link className="bg-green-700 text-white
+         text-center p-3 rounded-lg uppercase hover:opacity-95
+         " to={'/create-listing'}>
+          Create Listing
+        </Link>
       </form>
       <div className="flex justify-between mt-5">
         <span className="text-red-700 cursor-pointer" onClick={handleDeleteUser}>
